cacbf2a1e507d3c15ec6c5ec0c0e54789bec8fae
rickymwethera <rickymwethera@gmail.com>
2020-08-27 15:14:50 +0300

    initial commit

---
+++ src/app/meal.ts
@@ -0,0 +1,9 @@
+export class Meal {
+
+    constructor(
+        public id: number;
+        public mealname: string;
+        public description: string;
+        public submitter: string;
+    )
+}
